bc837c6ed11981c5651d2fd99318503b4654ab08
AbhinavArakkan <abhinavarakkan333@gmail.com>
2025-10-14 00:08:10 +0530

    Fix database migrations

--- .platform/hooks/postdeploy/01_collectstatic.sh
+++ .platform/hooks/postdeploy/01_collectstatic.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 source /var/app/venv/*/bin/activate
 cd /var/app/current
-python manage.py collectstatic --noinput
+
+rm -f db.sqlite3
+
 python manage.py migrate --noinput
+
+python manage.py collectstatic --noinput
