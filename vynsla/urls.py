bcc6bc3e06ef94e1e2ede39b52a2b07980ad75c2
AbhinavArakkan <abhinavarakkan333@gmail.com>
2025-10-14 15:52:11 +0530

    Configure nginx to serve media files

--- vynsla/urls.py
+++ vynsla/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('', include('home.urls')),
 ]
 
-if settings.DEBUG or True:  # Serve media in production too
+# Serve media files in development
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
