e33dcbba28c0e742458e7662118b1cbfdc086747
AbhinavArakkan <abhinavarakkan333@gmail.com>
2025-10-16 14:09:15 +0530

    Fix database permissions in all hooks

--- .platform/hooks/postdeploy/01_collectstatic.sh
+++ .platform/hooks/postdeploy/01_collectstatic.sh
@@ -10,6 +10,11 @@ python manage.py migrate --noinput
 python manage.py createsuperuser --noinput || echo "Superuser already exists!"
 python manage.py collectstatic --noinput --clear
 
+# CRITICAL: Fix database permissions!
+chmod 664 /var/app/current/db.sqlite3
+chmod 775 /var/app/current
+
+# Fix static/media permissions
 chmod -R 755 /var/app/current/staticfiles
 chmod -R 755 /var/app/current/media
 
--- .platform/hooks/prebuild/01_create_dirs.sh
+++ .platform/hooks/prebuild/01_create_dirs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p /var/app/staging/staticfiles
 mkdir -p /var/app/staging/media
-chmod -R 755 /var/app/staging/staticfiles
-chmod -R 755 /var/app/staging/media
+chmod -R 775 /var/app/staging
