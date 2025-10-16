22a571dcf3a990ac31cafe313024bee1c3426935
AbhinavArakkan <abhinavarakkan333@gmail.com>
2025-10-16 23:58:17 +0530

    Fix S3 ACL settings

--- vynsla/settings.py
+++ vynsla/settings.py
@@ -112,8 +112,9 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None  # Changed from 'public-read' to None
 AWS_LOCATION = 'media'
+
 
 # Use S3 for media files storage
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
